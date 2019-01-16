dad4dfc0ed48f39772d3ed541ecbea28c62c9390
Thiago Barradas <thiagobrowskt@gmail.com>
2019-01-16 17:48:05 -0200

    add appveyor ci

--- src/Nancy.Swagger.Annotations/Properties/AssemblyInfo.cs
+++ src/Nancy.Swagger.Annotations/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Nancy.Swagger.Annotations")]
+[assembly: AssemblyProduct("Nancy.Swagger.Annotations.JsonNET")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
